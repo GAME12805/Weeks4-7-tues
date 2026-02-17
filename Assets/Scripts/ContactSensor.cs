bd075399c7e63c7f2d7bf828b41ab0e847242f44
DESKTOP-111JM1H\Kit Barry <kit.barry@sheridancollege.ca>
2026-02-17 10:49:26 -0500

    Contact sensor code working

---
+++ Assets/Scripts/ContactSensor.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ContactSensor : MonoBehaviour
+{
+    public SpriteRenderer hazard;
+    public bool isInHazard = false;
+    public UnityEvent OnEnterHazard;
+    public UnityEvent OnExitHazard;
+
+    public UnityEvent<float> OnRandomNumber;
+  
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //is the player inside the hazard sprite?
+        if(hazard.bounds.Contains(transform.position) == true)
+        {
+            if(isInHazard == true)
+            {
+                //we're still in the harard
+            }
+            else
+            {
+                //jkust entered the hazard! do something
+                //Y: player has tripped the sensor
+                isInHazard = true;
+                Debug.Log("Entered the haraz!");
+                OnEnterHazard.Invoke();
+            }
+
+        }
+        else
+        {
+            if(isInHazard == true)
+            {
+                //we just left the hazard
+                // N: player has not tripped the sensor
+                isInHazard = false;
+                Debug.Log("Exited the haraz!");
+                OnExitHazard.Invoke();
+                OnRandomNumber.Invoke(Random.Range(0, 10));
+            }
+            else
+            {
+                //we're still not in the hazard
+            }
+            
+        }
+    }
+
+    public void ShowNumber(float number)
+    {
+        Debug.Log(number);
+    }
+}
